ce2d0fcb0cdaf21b78c0684739353243a0fa71cf
Erick Otenyo <otenyo.erick@gmail.com>
2020-11-10 16:52:06 +0300

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,12 @@
+const logger = require("logger");
+
+require("dotenv").config({ silent: true });
+
+require("app")().then(
+  () => {
+    logger.info("Server running");
+  },
+  (err) => {
+    logger.error("Error running server", err);
+  }
+);
---
+++ app/src/request.js
@@ -0,0 +1,10 @@
+const { create } = require("axios");
+
+const BASE_URL = process.env.MIKE_API_URL
+
+const TIMEOUT = process.env.TIMEOUT || 30 * 1000;
+
+exports.apiRequest = create({
+  timeout: TIMEOUT,
+  baseURL: BASE_URL,
+});
